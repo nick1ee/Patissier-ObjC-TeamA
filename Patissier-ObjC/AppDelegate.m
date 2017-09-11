a6aeb7fc32e6a38add08057af7ce2a9fc9154501
yuling <takusax@gmail.com>
2017-09-11 10:47:07 +0800

    init project

--- Patissier-ObjC/AppDelegate.m
+++ Patissier-ObjC/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+//    [self setWindow: [[UIWindow alloc] initwithFrame: [[UIScreen mainScreen] bounds]]]
+    
+    
+    
     return YES;
 }
 
