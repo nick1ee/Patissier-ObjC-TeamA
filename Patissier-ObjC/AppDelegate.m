c5edb2c256c621b0a0efcbf06e829dd9803822ee
yuling <takusax@gmail.com>
2017-09-12 14:26:15 +0800

    show gradient view

--- Patissier-ObjC/AppDelegate.m
+++ Patissier-ObjC/AppDelegate.m
@@ -19,6 +19,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     LandingViewController *initViewController = [[UIStoryboard storyboardWithName:@"Landing" bundle:nil] instantiateViewControllerWithIdentifier: @"LandingViewController"];
@@ -34,6 +36,17 @@
     
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                  openURL:url
+                                                        sourceApplication:sourceApplication
+                                                               annotation:annotation
+                    ];
+    
+    return handled;
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Patissier-ObjC/Landing/LandingViewController.m
+++ Patissier-ObjC/Landing/LandingViewController.m
@@ -16,10 +16,10 @@
 
 @implementation LandingViewController
 
-@synthesize backgroundImageView;
-@synthesize backgroundGradientView;
-@synthesize logoLabel;
-@synthesize signInWithFacebookButton;
+//@synthesize backgroundImageView;
+//@synthesize backgroundGradientView;
+//@synthesize logoLabel;
+//@synthesize signInWithFacebookButton;
 
 
 - (void)viewDidLoad {
@@ -27,57 +27,85 @@
     
     [self setupBackgroundImageView];
     
+    [self setupBackgroundGradientView];
+    
     [self setupLogoLabel];
     
     [self setupSignInWithFacebookButton];
     
-    [self.view addSubview: backgroundImageView];
-    
-    [self.backgroundImageView addSubview: logoLabel];
-    
-    [self.backgroundImageView addSubview: signInWithFacebookButton];
-    
 }
+
+//- (void)viewDidLayoutSubviews {
+//    [super viewDidLayoutSubviews];
+//    
+//     _gradientLayer.frame = backgroundGradientView.bounds;
+//    
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 - (void)setupBackgroundImageView {
     
-    backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+    _backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
     
-    backgroundImageView.image = [UIImage imageNamed: @"image-landing"];
+    _backgroundImageView.image = [UIImage imageNamed: @"image-landing"];
 
 }
 
 - (void)setupLogoLabel {
 
-    logoLabel.text = @"Patissier";
+    _logoLabel.text = @"Patissier";
     
-    logoLabel.font = [UIFont fontWithName: @"Georgia-Bold" size: 50.0];
+    _logoLabel.font = [UIFont fontWithName: @"Georgia-Bold" size: 50.0];
+    
+    _logoLabel.textColor = [UIColor whiteColor];
     
 }
 
 - (void)setupSignInWithFacebookButton {
-
+    
+    [_signInWithFacebookButton setTitle:@"Sign In With Facebook" forState:UIControlStateNormal];
+    
+    [_signInWithFacebookButton setBackgroundColor: [UIColor colorWithRed: 255/255.0 green: 53/255.0 blue: 71/255.0 alpha: 1.0]];
+    
+    [_signInWithFacebookButton.titleLabel setFont: [UIFont boldSystemFontOfSize: [UIFont systemFontSize]]];
+    
+    _signInWithFacebookButton.titleLabel.font = [UIFont systemFontOfSize: 16];
+   
 }
 
 - (void)setupBackgroundGradientView {
 
-    backgroundGradientView.alpha = 0.85;
+    _backgroundGradientView.alpha = 0.85;
     
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    
+    CAGradientLayer *gradientMask = gradientLayer;
+    
+    gradientMask.frame = self.view.bounds;
+    
+    gradientMask.colors = @[(id)[UIColor colorWithRed: 3/ 255.0 green: 63/ 255.0 blue: 122/ 255.0 alpha: 1.0].CGColor,
+                            (id)[UIColor colorWithRed: 4/ 255.0 green: 107/ 255.0 blue: 149/ 255.0 alpha: 1.0].CGColor];
+    
+    gradientMask.endPoint = CGPointMake(0.5, 1.0);
+    
+    gradientMask.startPoint = CGPointMake(0.5, 0.0);
+    
+//    [backgroundGradientView.layer addSublayer: gradientLayer];
+    
+    [_backgroundGradientView.layer insertSublayer: gradientLayer atIndex:0];
+    
+//    [backgroundGradientView addSubview: gradientMask];
     
 }
 
 - (IBAction)facebookLoginButtonPressed:(id)sender {
     
-    [signInWithFacebookButton setTitle:@"Sign In With Facebook Button" forState:UIControlStateNormal];
     
-//    [sender setTitle: @"Sign In With Facebook Button" forState: UIControlStateNormal];
     
-    signInWithFacebookButton.titleLabel.font = [UIFont fontWithName: @"UIFontWeightSemibold" size: 16.0];
     
 }
 
