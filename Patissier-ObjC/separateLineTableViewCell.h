0240b2c3c28f6aac185fe5c444b32f3b1087140e
yuling <takusax@gmail.com>
2017-09-13 16:37:50 +0800

    add separator line cell

---
+++ Patissier-ObjC/separateLineTableViewCell.h
@@ -0,0 +1,24 @@
+//
+//  separateLineTableViewCell.h
+//  Patissier-ObjC
+//
+//  Created by yuling on 2017/9/13.
+//  Copyright © 2017年 nicklee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface separateLineTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIView *topLeftSeparatorLineView;
+
+@property (weak, nonatomic) IBOutlet UIView *bottomLeftSeparatorLine;
+
+@property (weak, nonatomic) IBOutlet UIView *topRightSeparatorLineView;
+
+@property (weak, nonatomic) IBOutlet UIView *bottomRightSeparatorLine;
+
+@property (weak, nonatomic) IBOutlet UILabel *tittleLabel;
+
+
+@end
---
+++ Patissier-ObjC/separateLineTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  separateLineTableViewCell.m
+//  Patissier-ObjC
+//
+//  Created by yuling on 2017/9/13.
+//  Copyright © 2017年 nicklee. All rights reserved.
+//
+
+#import "separateLineTableViewCell.h"
+
+@implementation separateLineTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
